7dbf6cd556c58c1ec5454a1679f9cb375f984eb6
Work-PC\Work <gftvfx@gmail.com>
2017-03-17 22:56:50 -0700

    Mostly ready for deployment at this stage. Still a bit rough.
    Included toolbox as part of startup routine.
    
    Added a MaxLauncher.bat file that setups up the Environment Variables for the mxsLib

---
+++ MaxLauncher.bat
@@ -0,0 +1,13 @@
+set NETROOT=G:\
+set MXS_PATH=G:\repos\mxs_bundle\lib
+set MXS_CODEROOT=G:\repos\mxs_bundle
+set MXS_LIB=G:\repos\mxs_bundle\lib
+set MXS_TOOLS=G:\repos\mxs_bundle\tools
+set MXS_STARTUPPATH=G:\repos\mxs_bundle\lib\mxsLibMaxStartup.ms
+set MXS_ASSEMBLIES=G:\repos\mxs_bundle\assemblies
+
+set PYTHONPATH=C:\Program Files\Autodesk\3ds Max 2018;C:\Program Files\Autodesk\3ds Max 2018\python;dC:\Program Files\Autodesk\3ds Max 2018\python\Lib;C:\Program Files\Autodesk\3ds Max 2018\scripts\Python;C:\_cache\_scripts\_mxsLib\lib\python;C:\_cache\GavynPython
+
+"C:\Program Files\Autodesk\3ds Max 2018\3dsmax.exe"
+
+REM pause
---
+++ lib/scriptPlugins/README.txt
@@ -0,0 +1,3 @@
+Any script files in this root directory will be loaded by the startup routine.
+
+Assuming these files are scripted plugins that need to be available even in quiet mode. ( Rendering )
