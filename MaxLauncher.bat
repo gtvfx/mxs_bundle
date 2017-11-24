713ae0993402d8579d42abfe71a5a7e9ee9db165
Work-PC\Work <gftvfx@gmail.com>
2017-11-23 21:55:56 -0800

    Removed echo from bat file

--- MaxLauncher.bat
+++ MaxLauncher.bat
@@ -6,8 +6,6 @@ set MXS_STARTUPPATH=%MXSPATH%\_mxsStartup.ms
 
 set PYTHONPATH=%MXSPATH%\python;C:\Program Files\Autodesk\3ds Max 2018;C:\Program Files\Autodesk\3ds Max 2018\python;C:\Program Files\Autodesk\3ds Max 2018\python\Lib;C:\Program Files\Autodesk\3ds Max 2018\scripts\Python;C:\_cache\GavynPython
 
-echo %MXS_CODEROOT%"mxs_init.ms"
-
 xcopy /s/y "G:\repos\mxs_bundle\mxs_init.ms" "C:\Users\Work\AppData\Local\Autodesk\3dsMax\2018 - 64bit\ENU\scripts\startup"
 xcopy /s/y "G:\repos\mxs_bundle\mxs_props\MXS_EditorAbbrev.properties" "C:\Users\Work\AppData\Local\Autodesk\3dsMax\2018 - 64bit\ENU\MXS_EditorAbbrev.properties"
 xcopy /s/y "G:\repos\mxs_bundle\mxs_props\MXS_EditorUser.properties" "C:\Users\Work\AppData\Local\Autodesk\3dsMax\2018 - 64bit\MXS_EditorUser.properties"
