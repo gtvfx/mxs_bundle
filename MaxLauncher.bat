6ede2ae752949561c87d0285ffb0f14c42f31e76
Work-PC\Work <gftvfx@gmail.com>
2018-02-08 20:07:08 -0800

    updated to pull more data from expected environment variables

--- MaxLauncher.bat
+++ MaxLauncher.bat
@@ -1,15 +1,20 @@
+:: %DEV_PATH% is a user environment variable with a path to the code repository
+
+
+@echo off
+
 set NETROOT=G:\
-set MXS_CODEROOT=%NETROOT%repos\mxs_bundle
+set MXS_CODEROOT=%DEV_PATH%\mxs_bundle
 set MXSPATH=%MXS_CODEROOT%\lib
 set MXS_TOOLS=%MXSPATH%\tools
 set MXS_STARTUPPATH=%MXSPATH%\_mxsStartup.ms
+set MAXEXE="C:\Program Files\Autodesk\3ds Max 2018\3dsmax.exe"
 
 set PYTHONPATH=%MXSPATH%\python;C:\Program Files\Autodesk\3ds Max 2018;C:\Program Files\Autodesk\3ds Max 2018\python;C:\Program Files\Autodesk\3ds Max 2018\python\Lib;C:\Program Files\Autodesk\3ds Max 2018\scripts\Python;C:\_cache\GavynPython
 
-xcopy /s/y "G:\repos\mxs_bundle\mxs_init.ms" "C:\Users\Work\AppData\Local\Autodesk\3dsMax\2018 - 64bit\ENU\scripts\startup"
-xcopy /s/y "G:\repos\mxs_bundle\mxs_props\MXS_EditorAbbrev.properties" "C:\Users\Work\AppData\Local\Autodesk\3dsMax\2018 - 64bit\ENU\MXS_EditorAbbrev.properties"
-xcopy /s/y "G:\repos\mxs_bundle\mxs_props\MXS_EditorUser.properties" "C:\Users\Work\AppData\Local\Autodesk\3dsMax\2018 - 64bit\MXS_EditorUser.properties"
+xcopy /s/y "%DEV_PATH%\mxs_bundle\mxs_init.ms" "%LOCALAPPDATA%\Autodesk\3dsMax\2018 - 64bit\ENU\scripts\startup"
+xcopy /s/y "%DEV_PATH%\mxs_bundle\mxs_props\MXS_EditorAbbrev.properties" "%LOCALAPPDATA%\Autodesk\3dsMax\2018 - 64bit\ENU\MXS_EditorAbbrev.properties"
+xcopy /s/y "%DEV_PATH%\mxs_bundle\mxs_props\MXS_EditorUser.properties" "%LOCALAPPDATA%\Autodesk\3dsMax\2018 - 64bit\MXS_EditorUser.properties"
 
-"C:\Program Files\Autodesk\3ds Max 2018\3dsmax.exe"
 
-REM pause
+call %MAXEXE%
