f1d18cce4611e51469137f959dc600282812ee78
Work-PC\Work <gftvfx@gmail.com>
2018-03-18 16:43:39 -0700

    Really updated batch file...

--- MaxLauncher.bat
+++ MaxLauncher.bat
@@ -1,7 +1,7 @@
 :: %DEV_PATH% is a user environment variable with a path to the code repository
 
 
-@echo off
+:: @echo off
 
 set NETROOT=G:\
 set MXS_CODEROOT=%DEV_PATH%\mxs_bundle
@@ -10,7 +10,15 @@ set MXS_TOOLS=%MXSPATH%\tools
 set MXS_STARTUPPATH=%MXSPATH%\_mxsStartup.ms
 set MAXEXE="C:\Program Files\Autodesk\3ds Max 2018\3dsmax.exe"
 
-set PYTHONPATH=%MXSPATH%\python;C:\Program Files\Autodesk\3ds Max 2018;C:\Program Files\Autodesk\3ds Max 2018\python;C:\Program Files\Autodesk\3ds Max 2018\python\Lib;C:\Program Files\Autodesk\3ds Max 2018\scripts\Python;C:\_cache\GavynPython
+:: Set the PYTHONPATH to a clean environment with the bundle namespace as well as the 3dsmax paths
+:: You should be able to prepend or append any additional paths here
+:: Caret symbol (^) used to split lines
+set PYTHONPATH=^
+%MXSPATH%\python;^
+C:\Program Files\Autodesk\3ds Max 2018;^
+C:\Program Files\Autodesk\3ds Max 2018\python;^
+C:\Program Files\Autodesk\3ds Max 2018\python\Lib;^
+C:\Program Files\Autodesk\3ds Max 2018\scripts\Python
 
 xcopy /s/y "%DEV_PATH%\mxs_bundle\mxs_init.ms" "%LOCALAPPDATA%\Autodesk\3dsMax\2018 - 64bit\ENU\scripts\startup"
 xcopy /s/y "%DEV_PATH%\mxs_bundle\mxs_props\MXS_EditorAbbrev.properties" "%LOCALAPPDATA%\Autodesk\3dsMax\2018 - 64bit\ENU\MXS_EditorAbbrev.properties"
